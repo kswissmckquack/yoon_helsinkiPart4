8c65d3a6eb93af026349e68a120d8b65cdafcfac
Kevin Yoon <kevin.michael.yoon@gmail.com>
2020-05-26 21:15:03 -0400

    added mostBlogs.js function

---
+++ tests/mostBlogs.test.js
@@ -0,0 +1,18 @@
+const mostBlogs = require('../utils/mostBlogs');
+
+describe('most blogs', () => {
+  test('multiple blogs', () => {
+    const blogs = [
+      { author: 'james' },
+      { author: 'james' },
+      { author: 'james' },
+      { author: 'alphabet' },
+      { author: 'gym' },
+      { author: 'jones' },
+      { author: 'julio' },
+      { author: 'lilly' },
+    ];
+    const result = mostBlogs.mostBlogs(blogs);
+    expect(result).toEqual({ author: 'james', blogs: 3 });//use toEqual for objects
+  })
+})
